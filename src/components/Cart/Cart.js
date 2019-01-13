4311c4edca876c260558277c0285b61f6632c584
Khoi <khoi@Khois-MacBook-Pro.local>
2019-01-13 18:12:51 -0500

    paypal button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,34 +1,34 @@
-import React, { Component, Fragment } from 'react';
-import Title from '../Title';
-import CartColumns from './CartColumns';
-import EmptyCart from './EmptyCart';
-import { ProductConsumer } from '../../context';
-import CartList from './CartList';
-import CartTotals from './CartTotals';
+import React, { Component, Fragment } from "react";
+import Title from "../Title";
+import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from "../../context";
+import CartList from "./CartList";
+import CartTotals from "./CartTotals";
 
 export default class Cart extends Component {
-	render() {
-		return (
-			<section>
-				<ProductConsumer>
-					{value => {
-						const { cart } = value;
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
 
-						if (cart.length > 0) {
-							return (
-								<Fragment>
-									<Title name="your" title="cart" />
-									<CartColumns />
-									<CartList value={value} />
-									<CartTotals value={value} />
-								</Fragment>
-							);
-						} else {
-							return <EmptyCart />;
-						}
-					}}
-				</ProductConsumer>
-			</section>
-		);
-	}
+            if (cart.length > 0) {
+              return (
+                <Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
 }
